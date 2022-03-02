ee9415aaed8bb72fa870ae4316c7af5d3359d961
deeppines <atheus92@gmail.com>
2022-03-02 11:29:45 +0300

    refactor: updated button

---
+++ src/ui/elements/button/button.ts
@@ -0,0 +1,21 @@
+import Block from '../../../utils/Block';
+
+import template from './button.tpl.pug';
+
+interface IButton {
+  type: string;
+  name: string;
+  text: string;
+}
+
+class Button extends Block {
+  constructor(props: IButton) {
+    super(props);
+  }
+
+  render() {
+    return this.compile(template, { ...this.props });
+  }
+}
+
+export default Button;
