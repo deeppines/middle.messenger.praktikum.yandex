4a1535b57df366ea0a882c751824c7b1b34e31f8
deeppines <atheus92@gmail.com>
2022-03-04 15:26:41 +0300

    feat: added new page

--- src/pages/chat-active/chat-active.ts
+++ src/pages/chat-active/chat-active.ts
@@ -1,11 +1,29 @@
-import Block from '../../utils/Block';
-import renderDOM from '../../utils/renderDOM';
+import Block from '@/utils/Block';
+import renderDOM from '@/utils/renderDOM';
+
+import ChatFooter from '@/ui/components/chat-footer/chat-footer';
+import ChatHeader from '@/ui/components/chat-header/chat-header';
+import ChatList from '@/ui/components/chat-list/chat-list';
+import Header from '@/ui/components/header/header';
+import MessageView from '@/ui/components/message-view/message-view';
 
 import template from './chat-active.tpl.pug';
+import { data, messages } from './data';
 
 class ChatPage extends Block {
-  constructor() {
-    super();
+  protected initChildren() {
+    this.childrens.header = new Header();
+
+    this.childrens.chats = new ChatList({
+      data: data,
+    });
+
+    this.childrens.chatHeader = new ChatHeader();
+    this.childrens.chatFooter = new ChatFooter();
+
+    this.childrens.messageView = new MessageView({
+      data: messages,
+    });
   }
 
   render() {
