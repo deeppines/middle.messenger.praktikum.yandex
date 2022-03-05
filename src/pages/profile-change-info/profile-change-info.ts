6f48c3d8b0f6191a9b001f0484d4de6b7f39b3d9
deeppines <atheus92@gmail.com>
2022-03-05 11:26:37 +0300

    feat: added new page

--- src/pages/profile-change-info/profile-change-info.ts
+++ src/pages/profile-change-info/profile-change-info.ts
@@ -1,11 +1,13 @@
 import Block from '@/utils/Block';
 import renderDOM from '@/utils/renderDOM';
 
+import ProfileChangeInfo from '@/ui/components/profile-change-info/profile-change-info';
+
 import template from './profile-change-info.tpl.pug';
 
 class ProfilePage extends Block {
-  constructor() {
-    super();
+  protected initChildren() {
+    this.childrens.profileChangeInfo = new ProfileChangeInfo();
   }
 
   render() {
