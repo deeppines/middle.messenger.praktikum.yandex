ddaba542624eacf9e854d256f9b59c5e8cef11be
deeppines <atheus92@gmail.com>
2022-03-07 19:22:43 +0300

    feractor: fixed icon

--- src/ui/components/header/header.ts
+++ src/ui/components/header/header.ts
@@ -1,10 +1,13 @@
 import Block from '@/utils/Block';
 import { formDataToObject } from '@/utils/formDataToObject';
 
+import Icon from '@/ui/elements/icon/icon';
 import Link from '@/ui/elements/link/link';
 import Search from '@/ui/components/search/search';
 
 import template from './header.tpl.pug';
+
+import arrowRight from '@/assets/icons/arrow-right.svg';
 
 class Header extends Block {
   protected initChildren() {
@@ -12,6 +15,11 @@ class Header extends Block {
       url: '/profile/profile.html',
       name: 'Профиль',
       mod: 'link--grey link--icon',
+      block: new Icon({
+        id: arrowRight,
+        width: 6,
+        height: 10,
+      }),
     });
 
     this.childrens.search = new Search({
