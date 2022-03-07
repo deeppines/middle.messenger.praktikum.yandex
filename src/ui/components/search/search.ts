aa4b841486fb89803291f8e2aaed6173e3c82e7a
deeppines <atheus92@gmail.com>
2022-03-07 19:27:08 +0300

    feractor: fixed icon

--- src/ui/components/search/search.ts
+++ src/ui/components/search/search.ts
@@ -2,7 +2,11 @@ import { TEvents } from '@/types/common';
 
 import Block from '@/utils/Block';
 
+import Icon from '@/ui/elements/icon/icon';
+
 import template from './search.tpl.pug';
+
+import iconSearch from '@/assets/icons/icon-search.svg';
 
 interface ISearch {
   events?: TEvents;
@@ -10,6 +14,14 @@ interface ISearch {
 class Search extends Block {
   constructor(props: ISearch) {
     super(props);
+  }
+
+  protected initChildren() {
+    this.childrens.icon = new Icon({
+      id: iconSearch,
+      width: 20,
+      height: 20,
+    });
   }
 
   render() {
