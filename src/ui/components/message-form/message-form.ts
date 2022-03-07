34f33e205267ad3cb1bf8d72ae3c4cd4454e252f
deeppines <atheus92@gmail.com>
2022-03-07 13:04:28 +0300

    refactor: add validation for message

--- src/ui/components/message-form/message-form.ts
+++ src/ui/components/message-form/message-form.ts
@@ -2,7 +2,12 @@ import { TEvents } from '@/types/common';
 
 import Block from '@/utils/Block';
 
+import Button from '@/ui/elements/button/button';
+import Input from '@/ui/elements/input/input';
+
 import template from './message-form.tpl.pug';
+
+import { MESSAGE } from '@/constants';
 
 interface IMessageForm {
   events?: TEvents;
@@ -10,6 +15,23 @@ interface IMessageForm {
 class MessageForm extends Block {
   constructor(props: IMessageForm) {
     super(props);
+  }
+
+  protected initChildren(): void {
+    this.childrens.input = new Input({
+      type: 'text',
+      name: 'message',
+      placeholder: 'Сообщение',
+      classes: 'message-form__text',
+      required: true,
+      pattern: MESSAGE,
+    });
+
+    this.childrens.button = new Button({
+      type: 'submit',
+      name: 'send',
+      classes: 'button--rounded button--back message-form__send',
+    });
   }
 
   render() {
