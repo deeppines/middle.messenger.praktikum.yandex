2abef476aaf8f6f4f1e66d04c5237ba6d3f5902c
deeppines <atheus92@gmail.com>
2022-03-07 13:19:40 +0300

    refactor: added validation for profile pass

--- src/ui/components/profile-change-password/profile-change-password.ts
+++ src/ui/components/profile-change-password/profile-change-password.ts
@@ -3,10 +3,12 @@ import { TEvents } from '@/types/common';
 import Block from '@/utils/Block';
 
 import Button from '@/ui/elements/button/button';
-import Input from '@/ui/elements/input/input';
 import Link from '@/ui/elements/link/link';
+import FormField from '@/ui/components/form-field/form-field';
 
 import template from './profile-change-password.tpl.pug';
+
+import { PASSWORD } from '@/constants';
 
 interface IProfileChangePassword {
   events?: TEvents;
@@ -28,22 +30,46 @@ class ProfileChangePassword extends Block {
       text: 'Сохранить',
     });
 
-    this.childrens.oldPassword = new Input({
-      type: 'password',
-      name: 'oldPassword',
-      placeholder: '•••••••••',
+    this.childrens.oldPassword = new FormField({
+      inputProps: {
+        type: 'password',
+        name: 'password',
+        placeholder: '•••••••••',
+        required: true,
+        minlength: 8,
+        maxlength: 40,
+        pattern: PASSWORD,
+      },
+      classes: 'profile__field',
+      validate: true,
     });
 
-    this.childrens.newPassword = new Input({
-      type: 'password',
-      name: 'newPassword',
-      placeholder: '•••••••••••',
+    this.childrens.newPassword = new FormField({
+      inputProps: {
+        type: 'password',
+        name: 'newPassword',
+        placeholder: '•••••••••••',
+        required: true,
+        minlength: 8,
+        maxlength: 40,
+        pattern: PASSWORD,
+      },
+      classes: 'profile__field',
+      validate: true,
     });
 
-    this.childrens.newPasswordRepeat = new Input({
-      type: 'password',
-      name: 'newPassword',
-      placeholder: '•••••••••••',
+    this.childrens.newPasswordRepeat = new FormField({
+      inputProps: {
+        type: 'password',
+        name: 'newPassword',
+        placeholder: '•••••••••••',
+        required: true,
+        minlength: 8,
+        maxlength: 40,
+        pattern: PASSWORD,
+      },
+      classes: 'profile__field',
+      validate: true,
     });
   }
 
