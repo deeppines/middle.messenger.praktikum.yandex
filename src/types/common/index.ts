ea431c8cb5fdc778fe52ac6b3a3bd59d00a9d872
deeppines <atheus92@gmail.com>
2022-03-16 21:02:07 +0300

    feat: added common types

--- src/types/common/index.ts
+++ src/types/common/index.ts
@@ -1,1 +1,9 @@
+import Block from '@/utils/Block';
+
 export type TEvents = Record<string, (e: Event) => void>;
+
+export interface IPage {
+  path: string | string[];
+  block: typeof Block;
+  props?: Record<string, unknown>;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export * from './api';
 export * from './chat';
+export * from './common';
 export * from './messages';
 export * from './profile';
