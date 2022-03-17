ab143a11447c52213e6c2e5cb53133395fff6d3b
deeppines <atheus92@gmail.com>
2022-03-17 20:16:38 +0300

    feat: added Store

---
+++ src/utils/Store.ts
@@ -0,0 +1,24 @@
+import { Indexed } from '@/types';
+
+import EventBus from './EventBus';
+import { set } from './helpers';
+
+export enum StoreEvents {
+  Updated = 'updated',
+}
+
+class Store extends EventBus {
+  private state: Indexed = {};
+
+  public getState() {
+    return this.state;
+  }
+
+  public set(path: string, value: unknown) {
+    set(this.state, path, value);
+
+    this.emit(StoreEvents.Updated);
+  }
+}
+
+export default new Store();
