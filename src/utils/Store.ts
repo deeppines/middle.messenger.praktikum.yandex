d7a1293ab3227419b859ad37712505c69ef0b6e9
deeppines <atheus92@gmail.com>
2022-03-20 14:56:57 +0300

    refactor: update Store

--- src/utils/Store.ts
+++ src/utils/Store.ts
@@ -1,4 +1,4 @@
-import { Indexed } from '@/types';
+import { IUser } from '@/types';
 
 import EventBus from './EventBus';
 import { set } from './helpers';
@@ -7,18 +7,24 @@ export enum StoreEvents {
   Updated = 'updated',
 }
 
+export interface IState {
+  currentUser?: IUser;
+}
+
 class Store extends EventBus {
-  private state: Indexed = {};
+  private state: IState = {};
 
   public getState() {
     return this.state;
   }
 
-  public set(path: string, value: unknown) {
+  public set(path: keyof IState, value: unknown) {
     set(this.state, path, value);
 
     this.emit(StoreEvents.Updated);
   }
 }
 
-export default new Store();
+const store = new Store();
+
+export default store;
