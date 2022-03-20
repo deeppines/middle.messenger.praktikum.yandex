97be60b4bb455591d8f03fbd19ebfa6efb925d7a
deeppines <atheus92@gmail.com>
2022-03-20 16:57:07 +0300

    feat: added page paths constatnt

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export * from './api';
 export * from './common';
+export * from './pagePaths';
 export * from './pageRouting';
 export * from './validationRules';
--- src/constants/pageRouting.ts
+++ src/constants/pageRouting.ts
@@ -1,5 +1,7 @@
 import { IPage } from '@/types';
 
+import { PagePaths } from './pagePaths';
+
 import ClientErrorPage from '@/pages/404/404';
 import MessengerPage from '@/pages/messenger/messenger';
 import PasswordPage from '@/pages/password/password';
@@ -10,37 +12,37 @@ import SignupPage from '@/pages/signup/signup';
 
 export const PAGES: IPage[] = [
   {
-    path: ['/', '/signin'],
+    path: [PagePaths.Index, PagePaths.Signin],
     block: SigninPage,
     props: { title: 'Вход' },
   },
   {
-    path: '/signup',
+    path: PagePaths.Signup,
     block: SignupPage,
     props: { title: 'Регистрация' },
   },
   {
-    path: '/messenger',
+    path: PagePaths.Messanger,
     block: MessengerPage,
     props: { title: 'Чат' },
   },
   {
-    path: '/profile',
+    path: PagePaths.Profile,
     block: ProfilePage,
     props: { title: 'Профиль' },
   },
   {
-    path: '/settings',
+    path: PagePaths.Settings,
     block: SettingsPage,
     props: { title: 'Профиль - настройки' },
   },
   {
-    path: '/password',
+    path: PagePaths.Password,
     block: PasswordPage,
     props: { title: 'Профиль - пароль' },
   },
   {
-    path: '/404',
+    path: PagePaths.NotFound,
     block: ClientErrorPage,
     props: { title: 'Page Not Found - 404' },
   },
