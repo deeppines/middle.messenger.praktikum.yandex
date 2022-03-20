e049d96c97ac6c6b398362c05822e394e42f83d1
deeppines <atheus92@gmail.com>
2022-03-20 17:34:21 +0300

    refactor: updated index page

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,10 @@ import { ISignInData, ISignUpFormData } from '@/types';
 
 import AuthAPI from '@/api/AuthAPI';
 
+import { getPathname } from '@/utils/getPathname';
+
+import { PagePaths } from '@/constants';
+import router from '@/router/Router';
 import Store from '@/store/Store';
 
 class AuthController {
@@ -19,16 +23,53 @@ class AuthController {
     const { passwordCheck, ...signUpData } = data;
 
     await this.api.signUp(signUpData);
+    await this.getUser();
+
+    router.go(PagePaths.Profile);
   }
 
   async signIn(data: ISignInData) {
     await this.api.signIn(data);
+
+    await this.getUser();
+
+    router.go(PagePaths.Messanger);
   }
 
   async getUser() {
-    const user = await this.api.user();
+    const path = getPathname();
 
-    Store.set('currentUser', user);
+    try {
+      const user = await this.api.user();
+
+      Store.set('currentUser', user);
+
+      switch (path) {
+        case PagePaths.Signin:
+        case PagePaths.Signup:
+        case PagePaths.Index:
+          router.go(PagePaths.Profile);
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      switch (path) {
+        case PagePaths.Index:
+        case PagePaths.Signin:
+        case PagePaths.Signup:
+          break;
+        default:
+          router.go(PagePaths.Signin);
+          break;
+      }
+    }
+  }
+
+  async logout() {
+    await this.api.logout();
+
+    router.go('/');
   }
 }
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,19 +1,13 @@
 import { PAGES } from '@/constants';
 import AuthController from '@/controllers/AuthController';
-import Router from '@/router/Router';
+import router from '@/router/Router';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    await AuthController.getUser();
-  } catch (error) {
-    throw new Error(error as string);
-  }
-
-  const router = new Router();
-
   PAGES.forEach((page) => {
     router.use(page.path, page.block, page.props);
   });
 
   router.start();
+
+  await AuthController.getUser();
 });
