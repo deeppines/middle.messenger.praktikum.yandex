7c5ee84ef80967749141b13a1bac05ce2c90e6a6
deeppines <atheus92@gmail.com>
2022-03-20 15:02:16 +0300

    refactor: updated index page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,15 @@
 import Router from '@/utils/Router';
 
 import { PAGES } from '@/constants';
+import AuthController from '@/controllers/AuthController';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    await AuthController.getUser();
+  } catch (error) {
+    throw new Error(error as string);
+  }
+
   const router = new Router();
 
   PAGES.forEach((page) => {
