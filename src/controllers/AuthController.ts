d8119bfacad0eb904f65cf950e0946634f678131
deeppines <atheus92@gmail.com>
2022-03-20 15:00:37 +0300

    feat: added AuthController

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,35 @@
+import { ISignInData, ISignUpFormData } from '@/types';
+
+import AuthAPI from '@/api/AuthAPI';
+
+import Store from '@/utils/Store';
+
+class AuthController {
+  private api: AuthAPI;
+
+  constructor() {
+    this.api = new AuthAPI();
+  }
+
+  async signUp(data: ISignUpFormData) {
+    if (data.password !== data.passwordCheck) {
+      throw new Error('Пароли не совпадают');
+    }
+
+    const { passwordCheck, ...signUpData } = data;
+
+    await this.api.signUp(signUpData);
+  }
+
+  async signIn(data: ISignInData) {
+    await this.api.signIn(data);
+  }
+
+  async getUser() {
+    const user = await this.api.user();
+
+    Store.set('currentUser', user);
+  }
+}
+
+export default new AuthController();
