6f45709acee9164afc585dc04bc58b7b363d6346
deeppines <atheus92@gmail.com>
2022-03-20 17:00:23 +0300

    refactor: fixed router hoc

--- src/hoc/withRouter.ts
+++ src/hoc/withRouter.ts
@@ -1,13 +1,13 @@
 import Block from '@/utils/Block';
 
-import Router from '@/router/Router';
+import router from '@/router/Router';
 
 function withRouter(Component: typeof Block) {
   return class WithRouter extends Component {
     public static componentName = Component.name;
 
     constructor(props: any) {
-      super({ ...props, router: new Router() });
+      super({ ...props, router: router });
     }
   };
 }
