786c63b5c0137f7977e7e590e1c578f38f7d7d12
deeppines <atheus92@gmail.com>
2022-03-20 09:52:57 +0300

    feat: added auth interfaces

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -6,3 +6,28 @@ export type Options = {
 };
 
 export type OptionsWithoutMethod = Omit<Options, 'method'>;
+
+export interface ISignUpData {
+  first_name: string;
+  second_name: string;
+  login: string;
+  email: string;
+  password: string;
+  phone: string;
+}
+
+export interface ISignInData {
+  login: string;
+  password: string;
+}
+
+export interface IUser {
+  id: number;
+  first_name: string;
+  second_name: string;
+  display_name: string;
+  login: string;
+  email: string;
+  phone: string;
+  avatar: string;
+}
