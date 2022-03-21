13bdd9059dc962a0e926d3742eb466e96e3226ab
deeppines <atheus92@gmail.com>
2022-03-21 20:27:33 +0300

    feat: added new component

---
+++ src/ui/components/change-avatar/change-avatar.ts
@@ -0,0 +1,21 @@
+import { TEvents } from '@/types';
+
+import Block from '@/utils/Block';
+
+import template from './change-avatar.tpl.pug';
+
+interface IChangeAvatar {
+  events?: TEvents;
+}
+
+class ChangeAvatar extends Block {
+  constructor(props: IChangeAvatar) {
+    super(props);
+  }
+
+  protected render(): DocumentFragment {
+    return this.compile(template, {});
+  }
+}
+
+export default ChangeAvatar;
