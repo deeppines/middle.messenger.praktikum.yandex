477ca8a23442a4d7c498da3ccc14cb50b984268a
deeppines <atheus92@gmail.com>
2022-03-22 22:27:57 +0300

    feat: added change avatar form

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -2,12 +2,13 @@ import Block from '@/utils/Block';
 import { closeModal } from '@/utils/helpers';
 
 import Icon from '@/ui/elements/icon/icon';
-import ProfileChangeAvatar from '@/ui/components/change-avatar/change-avatar';
+import ChangeAvatar from '@/ui/components/change-avatar/change-avatar';
 import Profile from '@/ui/components/profile/profile';
 
 import template from './profile.tpl.pug';
 
 import arrowLeft from '@/assets/icons/arrow-left.svg';
+import UserController from '@/controllers/UserController';
 class ProfilePage extends Block {
   protected initChildren() {
     this.childrens.profile = new Profile({});
@@ -18,11 +19,21 @@ class ProfilePage extends Block {
       height: 24,
     });
 
-    this.childrens.changeAvatar = new ProfileChangeAvatar({
+    this.childrens.changeAvatar = new ChangeAvatar({
       events: {
-        click: (e) => closeModal(e, 'changeAvatar'),
+        click: (e) => closeModal('changeAvatar', e),
+        submit: (e) => this.submitHandler(e),
       },
     });
+  }
+
+  submitHandler(e: Event) {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    UserController.updateAvatar(formData);
   }
 
   render() {
