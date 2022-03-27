8f181495062ae82dc3692b2da57568664627950f
deeppines <atheus92@gmail.com>
2022-03-27 09:53:33 +0300

    refactor: moved mixins

--- src/pages/messenger/messenger.ts
+++ src/pages/messenger/messenger.ts
@@ -1,21 +1,52 @@
-import Block from '@/utils/Block';
+import { IChatCreate } from '@/types';
 
+import Block from '@/utils/Block';
+import { formDataToObject } from '@/utils/formDataToObject';
+import { closeModal } from '@/utils/helpers';
+
+import AddChat from '@/ui/components/add-chat/add-chat';
+import ChatFooter from '@/ui/components/chat-footer/chat-footer';
+import ChatHeader from '@/ui/components/chat-header/chat-header';
 import ChatList from '@/ui/components/chat-list/chat-list';
 import Header from '@/ui/components/header/header';
 
-import { data } from './data';
+// import MessageView from '@/ui/components/message-view/message-view';
 import template from './messenger.tpl.pug';
+
+import ChatsController from '@/controllers/ChatsController';
 
 class MessengerPage extends Block {
   protected initChildren() {
     this.childrens.header = new Header();
 
-    this.childrens.chats = new ChatList({
-      items: data,
+    this.childrens.chats = new ChatList({});
+
+    this.childrens.chatHeader = new ChatHeader();
+    this.childrens.chatFooter = new ChatFooter();
+
+    this.childrens.addChatModal = new AddChat({
+      events: {
+        click: (e) => closeModal('addChat', e),
+        submit: (e) => this.submitHandler(e),
+      },
     });
+
+    // this.childrens.messageView = new MessageView({data: ''});
+  }
+
+  submitHandler(e: Event) {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const data = formDataToObject(formData);
+
+    ChatsController.addChat(data as unknown as IChatCreate);
   }
 
   render() {
+    ChatsController.getChats();
+
     return this.compile(template, {});
   }
 }
