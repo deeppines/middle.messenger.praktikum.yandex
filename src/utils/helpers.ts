35973388bb3e5e4e9a6c172e17bfd966e1a72b57
deeppines <atheus92@gmail.com>
2022-06-06 18:24:56 +0300

    refactor: разнес часть утилит по отдельным файлам

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,3 @@
-import { Indexed } from 'src/types';
-
 export const addClass = (cl: string, el: HTMLElement | null): void => {
   if (el) el.classList.add(cl);
 };
@@ -18,97 +16,6 @@ export const inputHasValue = (input?: HTMLInputElement | null): boolean => {
   return false;
 };
 
-export const isEqual = (a: Record<string, unknown>, b: Record<string, unknown>): boolean => {
-  if (a === null || a === undefined || b === null || b === undefined) return a === b;
-
-  if (a.constructor !== b.constructor) return false;
-
-  if (a instanceof Function) return a === b;
-
-  if (a instanceof RegExp) return a === b;
-
-  if (a === b || a.valueOf() === b.valueOf()) return true;
-
-  if (Array.isArray(a) && Array.isArray(b) && a.length !== b.length) return false;
-
-  if (a instanceof Date) return false;
-
-  if (!(a instanceof Object)) return false;
-  if (!(b instanceof Object)) return false;
-
-  const p = Object.keys(a);
-
-  return (
-    Object.keys(b).every((i) => {
-      return p.indexOf(i) !== -1;
-    }) &&
-    p.every((i) => {
-      return isEqual(a[i] as Record<string, unknown>, b[i] as Record<string, unknown>);
-    })
-  );
-};
-
-export const isObject = (item: Indexed<any> | undefined): boolean => {
-  if (!item) return false;
-
-  return item && typeof item === 'object' && !Array.isArray(item);
-};
-
-export const merge = (lhs: Indexed<any>, rhs: Indexed<any>): Indexed<any> => {
-  if (!rhs) return lhs;
-
-  const rhsArr: Indexed<any>[] = [rhs];
-
-  if (!rhsArr.length) return lhs;
-
-  const src = rhsArr.shift();
-
-  if (isObject(lhs) && isObject(src)) {
-    for (const key in src) {
-      if (isObject(src[key])) {
-        if (!lhs[key]) {
-          Object.assign(lhs, {
-            [key]: {},
-          });
-        }
-
-        merge(lhs[key], src[key]);
-      } else {
-        Object.assign(lhs, {
-          [key]: src[key],
-        });
-      }
-    }
-  }
-
-  return rhsArr[0] ? merge(lhs, rhsArr[0]) : lhs;
-};
-
-export const createNestedObj = (source: Indexed<any>, target = {}): Indexed<any> => {
-  const result = Object.entries(source).reduce((obj: Indexed<any>, [k, v]) => {
-    const path = k.split('.');
-    const prop = path.pop();
-
-    if (prop) {
-      path.reduce((acc, n: string) => (acc[n] = acc[n] || {}), obj)[prop] = v;
-    }
-
-    return obj;
-  }, target);
-
-  return result;
-};
-
-export const set = (object: Indexed<any>, path: string, value: unknown): Indexed<any> => {
-  if (typeof path !== 'string') throw new Error('path must be string');
-  if (!isObject(object)) return object;
-
-  const baseObject = { [path]: value };
-  const newObject = createNestedObj(baseObject);
-
-  return merge(object, newObject);
-};
-
 export const openModal = (id: string): void => {
   const modal = document.getElementById(id);
 
@@ -125,49 +32,4 @@ export const closeModal = (id: string, event?: Event): void => {
 
   if (target === modal) removeClass('active', modal);
   if (target === null) removeClass('active', modal);
-};
-
-export const isPlainObject = (value: unknown): value is Indexed => {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    value.constructor === Object &&
-    Object.prototype.toString.call(value) === '[object Object]'
-  );
-};
-
-export const isArray = (value: unknown): value is [] => {
-  return Array.isArray(value);
-};
-
-export const isArrayOrObject = (value: unknown): value is [] | Indexed => {
-  return isPlainObject(value) || isArray(value);
-};
-
-export const getKey = (key: string, parentKey?: string) => {
-  return parentKey ? `${parentKey}[${key}]` : key;
-};
-
-export const getParams = (data: Indexed | [], parentKey?: string) => {
-  const result: [string, string][] = [];
-
-  for (const [key, value] of Object.entries(data)) {
-    if (isArrayOrObject(value)) {
-      result.push(...getParams(value, getKey(key, parentKey)));
-    } else {
-      result.push([getKey(key, parentKey), encodeURIComponent(String(value))]);
-    }
-  }
-
-  return result;
-};
-
-export const queryString = (data: Indexed) => {
-  if (!isPlainObject(data)) {
-    throw new Error('input must be an object');
-  }
-
-  return `?${getParams(data)
-    .map((arr) => arr.join('='))
-    .join('&')}`;
 };
